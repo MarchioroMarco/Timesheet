30f9598281a1378ed3e6253e3111645371acee9b
ITS Adrian Manuc <ITSManucA@itsacademy.corvallisholding.it>
2020-02-12 16:47:59 +0100

    registrazione page

--- src/app/pages/registrazione/registrazione.component.ts
+++ src/app/pages/registrazione/registrazione.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DomainService } from 'src/app/api/core/services/domain.service';
 
 @Component({
   selector: 'app-registrazione',
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrazione.component.scss']
 })
 export class RegistrazioneComponent implements OnInit {
-
-  constructor() { }
+  
+  public formGroup:FormGroup;
+  constructor(public fb: FormBuilder, public router:Router, public domain:DomainService) { }
 
   ngOnInit() {
+    this.formGroup = this.fb.group({
+      username:['', [Validators.required]],
+      password:['', [Validators.required]]
+    });
   }
 
 }
